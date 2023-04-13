376580c531fdf10d65e997697d982364570147f3
anon <shinyhero777@gmail.com>
2023-04-14 01:41:23 +0300

    replaced all direct references to User model except Profile
    changed serializers to display tags in list view and commentators profile picture in detailed view

--- ImageShare/imageapp/models.py
+++ ImageShare/imageapp/models.py
@@ -43,7 +43,7 @@ def auto_delete_image_on_change(sender, instance, **kwargs):
 
 
 class Image(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images')
     description = models.CharField(max_length=1000, blank=True)
     active = models.BooleanField(default=False)
@@ -94,12 +94,12 @@ class Tag(models.Model):
 
 
 class Comment(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
     text = models.CharField(max_length=1000)
 
 
 class Report(models.Model):
-    byUser = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="by_user")
-    reportedUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reported_user")
+    byUser = models.ForeignKey(Profile, on_delete=models.DO_NOTHING, related_name="by_user")
+    reportedUser = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="reported_user")
     text = models.CharField(max_length=1000)
--- ImageShare/imageapp/serializers.py
+++ ImageShare/imageapp/serializers.py
@@ -3,21 +3,45 @@ from imageapp.models import Image, Tag, Comment, Report, Profile
 from django.contrib.auth.models import User
 
 
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    username = serializers.SlugRelatedField(
+        source='user',
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Profile
+        fields = ['user', 'username', 'picture']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'profile', 'username']
+
+
 class TagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tag
         fields = ['url', 'name', 'images']
+        extra_kwargs = {
+            'images': {'write_only': True},
+        }
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    owner = ProfileSerializer(read_only=True)
+
     class Meta:
         model = Comment
-        fields = ['url', 'owner', 'image', 'text']
+        fields = ['owner', 'text']
 
 
 class ImageListSerializer(serializers.HyperlinkedModelSerializer):
-    tag_set = TagSerializer(write_only=True, many=True)
-
+    tag_set = TagSerializer(many=True)
     class Meta:
         model = Image
         fields = ['url', 'owner', 'image', 'description', 'tag_set']
@@ -45,17 +69,3 @@ class ReportSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Report
         fields = ['url', 'byUser', 'reportedUser', 'text']
-
-
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['url', 'user', 'picture']
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    profile = ProfileSerializer(read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['url', 'profile', 'username']
