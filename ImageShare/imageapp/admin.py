9d9f1428eb00f4ff71983bf343b76e79f4819c77
anon <shinyhero777@gmail.com>
2023-10-31 17:11:57 +0200

    registered models with build-in admin interface

--- ImageShare/imageapp/admin.py
+++ ImageShare/imageapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from imageapp.models import Image, Tag, Comment, Report, Profile
 
-# Register your models here.
+admin.site.register(Image)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Report)
+admin.site.register(Profile)
+
