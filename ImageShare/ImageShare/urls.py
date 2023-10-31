23c17b616aa2bfd6a197d93b15e637fed6bd6f6e
anon <shinyhero777@gmail.com>
2023-10-31 17:20:56 +0200

    changed url path to admin interface

--- ImageShare/ImageShare/urls.py
+++ ImageShare/ImageShare/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('imageapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
