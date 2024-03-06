e214b573e5b52a912b103502ff091e946ec013d2
anon <shinyhero777@gmail.com>
2024-03-06 15:03:08 +0200

    switched from sqlite to postgresql

--- ImageShare/ImageShare/settings.py
+++ ImageShare/ImageShare/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'ImageShare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'PASSWORD': 'example',
+        'OPTIONS': {
+            'service': 'db',
+            'passfile': '.pwdfile',
+        },
     }
 }
 
