8393ba40fc75f0eb84366c55cc4d6eaf96cad4be
anon <shinyhero777@gmail.com>
2024-03-06 17:56:27 +0200

    refactored file structure

--- ImageShare/ImageShare/settings.py
+++ ImageShare/ImageShare/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = '/var/www/static/'  # BASE_DIR / 'static'  #
-STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/django-static/'  # BASE_DIR / 'static'  #
+STATIC_URL = '/django-static/'
 
 # User uploaded files
 
